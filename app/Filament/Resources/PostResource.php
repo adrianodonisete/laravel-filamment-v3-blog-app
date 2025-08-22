ea9b4dcd1bce56beabfd2b9f6415744ceffea501
Adriano D Machado <adriano@systax.com.br>
2025-08-22 14:42:26 -0300

    tabs and table filters

--- app/Filament/Resources/PostResource.php
+++ app/Filament/Resources/PostResource.php
@@ -6,6 +6,7 @@ use App\Filament\Resources\PostResource\Pages;
 use App\Filament\Resources\PostResource\RelationManagers;
 use App\Models\Category;
 use App\Models\Post;
+use BladeUI\Icons\Components\Icon;
 use Filament\Tables\Actions\ActionGroup;
 use Filament\Tables\Actions\DeleteAction;
 use Filament\Tables\Actions\EditAction;
@@ -19,16 +20,22 @@ use Filament\Forms\Components\Group;
 use Filament\Forms\Components\MarkdownEditor;
 use Filament\Forms\Components\Section;
 use Filament\Forms\Components\Select;
+use Filament\Forms\Components\Tabs;
+use Filament\Forms\Components\Tabs\Tab;
 use Filament\Forms\Components\TagsInput;
 use Filament\Forms\Components\TextInput;
 use Filament\Forms\Form;
 use Filament\Resources\Resource;
+use Filament\Support\Enums\IconPosition;
 use Filament\Tables;
 use Filament\Tables\Columns\CheckboxColumn;
 use Filament\Tables\Columns\ColorColumn;
 use Filament\Tables\Columns\ImageColumn;
 use Filament\Tables\Columns\TextColumn;
 use Filament\Tables\Enums\ActionsPosition;
+use Filament\Tables\Filters\Filter;
+use Filament\Tables\Filters\SelectFilter;
+use Filament\Tables\Filters\TernaryFilter;
 use Filament\Tables\Table;
 use Illuminate\Database\Eloquent\Builder;
 use Illuminate\Database\Eloquent\SoftDeletingScope;
@@ -43,52 +50,53 @@ class PostResource extends Resource
     {
         return $form
             ->schema([
-                Section::make('Create a Post')
-                    ->description('Fill in the details below to create a new post.')
-                    ->collapsible()
-                    ->columns(2)
-                    ->columnSpan(2)
-                    ->schema([
-                        TextInput::make('title')
-                            ->minLength(3)
-                            ->maxLength(50)
-                            ->required(),
-                        TextInput::make('slug')
-                            ->minLength(3)
-                            ->maxLength(100)
-                            ->unique(ignoreRecord: true)
-                            ->required(),
+                Tabs::make('Create new Post')
+                    ->tabs([
+                        Tab::make('Title')
+                            ->icon('heroicon-m-inbox')
+                            ->iconPosition(IconPosition::After)
+                            ->schema([
+                                TextInput::make('title')
+                                    ->minLength(3)
+                                    ->maxLength(50)
+                                    ->required(),
+                                TextInput::make('slug')
+                                    ->minLength(3)
+                                    ->maxLength(100)
+                                    ->unique(ignoreRecord: true)
+                                    ->required(),
 
-                        Select::make('category_id')
-                            ->label('Category')
-                            // ->options(Category::all()->pluck('name', 'id'))
-                            ->relationship('category', 'name')
-                            ->searchable()
-                            ->required(),
+                                Select::make('category_id')
+                                    ->label('Category')
+                                    // ->options(Category::all()->pluck('name', 'id'))
+                                    ->relationship('category', 'name')
+                                    ->searchable()
+                                    ->required(),
 
-                        ColorPicker::make('color')->required(),
-                        MarkdownEditor::make('content')
-                            ->required()
-                            ->columnSpanFull(),
-                    ]),
+                                ColorPicker::make('color')->required(),
+                            ]),
 
-                Group::make()
-                    ->schema([
-                        Section::make('Image')
-                            ->collapsible()
+                        Tab::make('Content')
+                            ->icon('heroicon-m-document-text')
+                            ->iconPosition(IconPosition::After)
+                            ->schema([
+                                MarkdownEditor::make('content')
+                                    ->required()
+                                    ->columnSpanFull(),
+                            ]),
+
+                        Tab::make('Meta')
                             ->schema([
                                 FileUpload::make('thumbnail')
                                     ->disk('public')
                                     ->directory('thumbnails')
                                     ->required(),
-                            ]),
 
-                        Section::make('Meta')
-                            ->schema([
                                 TagsInput::make('tags')->required(),
                                 Checkbox::make('published'),
                             ]),
-                        Section::make('Authors')
+
+                        Tab::make('Authors')
                             ->schema([
                                 Select::make('authors')
                                     ->label('Select Authors')
@@ -96,9 +104,10 @@ class PostResource extends Resource
                                     ->multiple()
                                     ->required()
                             ]),
-                    ]),
-
-
+                    ])
+                    ->columnSpanFull()
+                    ->activeTab(1)
+                    ->persistTabInQueryString(),
             ])
             ->columns([
                 'md' => 3,
@@ -147,7 +156,26 @@ class PostResource extends Resource
                     ->toggleable(),
             ])
             ->filters([
-                //
+                // Filter::make('published')
+                //     ->label('Published Posts')
+                //     ->query(fn(Builder $query): Builder => $query->where('published', true)),
+                // Filter::make('unpublished')
+                //     ->label('UnPublished Posts')
+                //     ->query(fn(Builder $query): Builder => $query->where('published', false)),
+
+                TernaryFilter::make('published')
+                    ->label('Published')
+                    ->trueLabel('Yes')
+                    ->falseLabel('No')
+                // ->query(fn(Builder $query, ?bool $value): Builder => $query->where('published', $value))
+                ,
+
+                SelectFilter::make('category_id')
+                    ->label('Category')
+                    // ->options(Category::all()->pluck('name', 'id'))
+                    ->relationship('category', 'name')
+                    ->multiple()
+                    ->preload(),
             ])
             ->actions(
                 [
